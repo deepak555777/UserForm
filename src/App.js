2b06f076d201ec3dadebcef7b95ae5a6b186965d
Deepak <deepak9478264417@gmail.com>
2023-01-23 05:36:18 -0800

    Added input validations to form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
+import AddUser from './Components/AddUser'
+import UserList from './Components/UserList'
 
 
 function App() {
+const [Users,setusers] = useState([])
+
+const handleUserData=(userName,userAge)=>{
+  setusers((prevUsers)=>{
+    
+    return [
+      ...prevUsers,
+      {name:userName,age:userAge},
+    ]
+  
+  })
+  console.log(Users);
+}
+
   return (
     <div>
-
+    <AddUser User={handleUserData}/>
+    <UserList users={Users}/>
     </div>
   );
 }
--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -2,43 +2,73 @@ import React,{useState} from 'react'
 import Classes from './AddUser.module.css';
 import Card from './UI/Card'
 import Button from './UI/Button'
+import ErrorModal from './UI/ErrorModal'
 
 function AddUser(props) {
-    const [userName, setuserName] = useState('')
-    const [userAge, setuserAge] = useState('')
-    const UserNameHandler=(event)=>{
-      setuserName(event.target.value)
-    }
-    const UserAgeHandler=(event)=>{
-        setuserAge(event.target.value)
-      }
+    const [userName, setuserName] = useState('');
+    const [userAge, setuserAge] = useState('');
+    const [error, setError] = useState();
+
 
       const SubmitHandler=(e)=>{
          e.preventDefault();
+         if (userName.trim().length === 0 || userAge.trim().length === 0) {
+            setError({
+              title: 'Invalid input',
+              message: 'Please enter a valid name and age (non-empty values).',
+            });
+            return;
+        }
+          if (+userAge < 1) {
+            setError({
+              title: 'Invalid age',
+              message: 'Please enter a valid age (> 0).',
+            });
+            return;
+        }
          props.User(userName,userAge);
+         setuserName('');
+         setuserAge('');
       }
+      const userNameHandler=(event)=>{
+        setuserName(event.target.value)
+      }
+      const userAgeHandler=(event)=>{
+          setuserAge(event.target.value)
+        }
+  
 
-      
+      const errorHandler = () => {
+        setError(null);
+      };
 
   return (
+    <div>
+    {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        /> ) }
     <Card className={Classes.input} >
         <form  onSubmit={SubmitHandler}>
             <label>Username</label>
             <input 
             type="UserName"
-            onChange={UserNameHandler}
+            onChange={userNameHandler}
             value={userName}
             ></input>
             <label>UserAge</label>
             <input 
             type="UserAge"
-            onChange={UserAgeHandler}
+            onChange={userAgeHandler}
             value={userAge}
             ></input>
-        <Button type="submit" >Add User</Button>
+        <Button type="submit">Add User</Button>
 
         </form>
         </Card>
+        </div>
   )
 }
 
